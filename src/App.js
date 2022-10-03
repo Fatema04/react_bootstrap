5dc2a157f2471a497fa79ddd6da46fd0f465036c
Fatema04 <shadrinmoni15@gmail.com>
2022-10-03 10:56:32 -0700

    card added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import GridExample from './components/Cards/Cards';
+
+
+function App() {
+  return (
+    <div className="App">
+      <GridExample></GridExample>
+
+
+    </div>
+  );
+}
+
+export default App;
